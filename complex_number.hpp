a93eaa4bcb164b5e7256470679b5205838091c4a
Marco <modasso23@icloud.com>
2025-04-01 23:56:02 +0200

    Adding the overloading of the operators and the concept

--- complex_number.hpp
+++ complex_number.hpp
@@ -1,62 +1,137 @@
 #pragma once
 
 /*This code creates a class for complex numbers expressed in the algebric form, which is:
+
                                     z = a + i*b 
+
 Where:
 - a is the real part of the complex number
 - b is the real part of the complex number
 - i = sqrt(-1) is the imaginary unit 
 */
 
-template<typename t>
+template<typename type> requires std::floating_point<type>
 class complex_number{
-    t real;
-    t imaginary;
+    // The attributes of a complex number are its real and its imaginary part
+    type real;
+    type imaginary;
 
 public:
+
     //Creating the default constructor that initalises the complex number to z = 0 + i*0 = 0 
     complex_number()
         : real(0), imaginary(0)
     {}
 
     /* Creating the user-defined constructor that initalises the complex number to z = re + i*im,
-     where <re> and <im> are numbers */
-    complex_number(t re, t im)
+     where <re> and <im> are floating_point numbers */
+    complex_number(type re, type im)
         : real(re), imaginary(im)
     {}
 
     /*Creating a user-defined constructor for the instances where we want to create a complex number with just the real part,
      therefore we use the `explicit` keyword in order to avoid creating a "converting constructor" */
-    explicit complex_number(t re)
+    explicit complex_number(type re)
         : real(re), imaginary(0)
     {}
 
     //Creating a method that gives the real part of the complex number as its output 
-    t give_real(void) const{
+    type give_real(void) const{
         return real;
     }
 
     //Creating a method that gives the imaginary part of the complex number as its output
-    t give_imaginary(void) const{
+    type give_imaginary(void) const{
         return imaginary;
     }
 
     //Creating a method that gives the conjugate of the complex number as its output 
     complex_number conjugate(void) const {
-        return complex_number(real,-imaginary);
+        return complex_number(real, -imaginary);
     }
+
+    //Overloading the operator+= in order to sum properly two complex numbers 
+    complex_number& operator+=(const complex_number& other){
+        type a = real;
+        type b = imaginary;
+        type c = other.give_real();
+        type d = other.give_imaginary();
+        real = a + c ;
+        imaginary = b + d ;
+        return *this;
+    }
+
+    // Overloading the operator+ using the operator+= defined above
+    complex_number operator+(const complex_number& other) const{
+        complex_number cn = *this;
+        cn += other;
+        return cn;
+    }
+
+    // Overloading the operator+= and the operator+ in order to sum properly a complex number and a float/double
+    complex_number& operator+=(const type& other){
+        real += other;
+        return *this;
+    }
+    complex_number operator+(const type& other) const{
+        complex_number cn = *this;
+        cn += other;
+        return cn;
+    }
+
+    //Overloading the operator*= in order to multiply properly two complex numbers 
+    complex_number& operator*=(const complex_number& other){
+        type a = real;
+        type b = imaginary;
+        type c = other.give_real();
+        type d = other.give_imaginary();
+        real = a * c - b * d ;
+        imaginary = b * c + a * d ;
+        return *this;
+    }
+
+    // Overloading the operator* using the operator*= defined above
+    complex_number operator*(const complex_number& other) const{
+        complex_number cn = *this;
+        cn *= other;
+        return cn;
+    }
+
+    // Overloading the operator*= and the operator* in order to multiply properly a complex number and a float/double
+    complex_number& operator*=(const type& other){
+        real *= other;
+        imaginary *= other;
+        return *this;
+    }
+    complex_number operator*(const type& other) const{
+        complex_number cn = *this;
+        cn *= other;
+        return cn;
+    }
+
 };
 
+//Overloading the operator+ and the operator* so that their commutativity is preserved
+template<typename type> requires std::floating_point<type>
+complex_number<type> operator+(const type& num, const complex_number<type>& cn){
+    // We simply invert the order of the factors
+    return cn + num;
+}
+template<typename type> requires std::floating_point<type>
+complex_number<type> operator*(const type& num, const complex_number<type>& cn){
+    return cn * num;
+}
+
 /*Overloading the operator<< in order to print the complex numbers the correct way*/
-template<typename t>
-    std::ostream& operator<<(std::ostream& os, const complex_number<t>& cn) {
-    t im_part = cn.give_imaginary();
+template<typename type> requires std::floating_point<type>
+    std::ostream& operator<<(std::ostream& os, const complex_number<type>& cn) {
+    type im_part = cn.give_imaginary();
     if (im_part != 0){
         // When the imaginary part is positive, we ought to add the sign before the value
         if (im_part > 0){
             os << cn.give_real() << "+" << cn.give_imaginary() << "i";
         }
-        // When the imaginary part is positive, we ought to add the sign before the valu
+        // When the imaginary part is positive, we ought to add the sign before the value
         else{
             os << cn.give_real() << cn.give_imaginary() << "i";
         }
--- main.cpp
+++ main.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
 #include "complex_number.hpp"
 
+using namespace std;
+
 int main(void){
     complex_number<float> cn1(2,5);
-    complex_number<float> cn2(1,-3);
-    std::cout << "cn1 is: " << cn1 << " and cn2 is: " << cn2 << std::endl;
-    std::cout << "The real parts of each one are: " << cn1.give_real() << " and " << cn2.give_real() << std::endl;
-    std::cout << "The imaginary parts of each one are: " << cn1.give_imaginary() << " and " << cn2.give_imaginary() << std::endl;
-    std::cout << "The conjugates are: " << cn1.conjugate() << " and " << cn2.conjugate() << std::endl;
+    complex_number<float> cn2(3,7);
+    complex_number<double> cn3(4);
+    cout << "cn1 is: " << cn1 << ", cn2 is: " << cn2 << " and cn3 is: " << cn3 << endl;
+    cout << "The real parts of each one are: " << cn1.give_real() << ", " << cn2.give_real() << " and " << cn3.give_real() << endl;
+    cout << "The imaginary parts of each one are: " << cn1.give_imaginary() << ", " << cn2.give_imaginary() 
+                << " and " << cn3.give_imaginary() << endl;
+    cout << "The conjugates are: " << cn1.conjugate() << ", " << cn2.conjugate() << " and " << cn3.conjugate() << endl;
+    float f = 1.248;
+    double d = 2.4816;
+    cout << "The float number is f: " << f << " , while the double number is d: " << d << endl;
+    cout << "The sum of cn1 and cn2 is: " << cn1 + cn2 << " ; their product is: " << cn1 * cn2 << endl;
+    cout << "The sum of cn1 and f is: " << cn1 + f << " , which is the same as the sum of f and cn1: " << f + cn1 << endl;
+    cout << "The sum of cn3 and d is: " << cn3 + d << " , which is the same as the sum of d and cn3: " << d + cn3 << endl;
+    cout << "The product of cn1 and f is: " << cn1 * f << " , which is the same as the product of f and cn1: " << f * cn1 << endl;
+    cout << "The product of cn3 and d is: " << cn3 * d << " , which is the same as the product of d and cn3: " << d * cn3 << endl;
     return 0;
 }
